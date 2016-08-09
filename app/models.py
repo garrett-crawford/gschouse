59da59ca1cb11b32b6dea812c9191b735554ce85
Garrett Crawford <gschouse@gmail.com>
2016-08-08 19:52:32 -0700

    Made several database changes to fix a unique constraint error. Also added all players to the database.

--- app/models.py
+++ app/models.py
@@ -3,9 +3,9 @@ from app import db
 class Player(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	nickname = db.Column(db.String(32), index = True, unique = True)
-	main = db.Column(db.String(32), index = True, unique = False)
+	main = db.Column(db.String(32), index = True)
 	ranking = db.Column(db.Integer, index = True, unique = True)
-	description = db.Column(db.String(512), index = True, unique = True)
+	description = db.Column(db.String(512), index = True)
 
 	def __repr__(self):
 		return '<Player %r>' % (self.nickname)
